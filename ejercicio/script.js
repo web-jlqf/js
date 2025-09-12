38c69f11e6d41fb352033c84888421052d606ade
José Luis <luis.quiroz@uacm.edu.mx>
2025-09-11 19:10:55 -0600

    Agregando el primer ejercicio de calculadora

---
+++ ejercicio/script.js
@@ -0,0 +1,24 @@
+/* 
+  Ejercicio: Implementa la lógica de cada operación.
+  Sugeridos:
+  - Lee A (#a) y B (#b) con parseFloat.
+  - Valida (NaN, división por 0).
+  - Escribe el resultado en #result.
+  - (Opcional) Mensajes en #msg.
+*/
+
+function calcAdd(){
+  // TODO: sumar A + B y mostrar en #result
+}
+
+function calcSub(){
+  // TODO: restar A - B y mostrar en #result
+}
+
+function calcMul(){
+  // TODO: multiplicar A * B y mostrar en #result
+}
+
+function calcDiv(){
+  // TODO: dividir A / B (validar B != 0) y mostrar en #result
+}
